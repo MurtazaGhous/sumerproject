930252cfe373d728b64cac3133a4ceb8d67fc08b
Ghous <ghousm1@vdi-scclab-002.lancs.local>
2023-05-20 22:36:45 +0100

    Class used to handle controls and collision

---
+++ GameLogic.java
@@ -0,0 +1,264 @@
+import java.awt.event.KeyEvent;
+
+public class GameLogic {
+    
+    private GameArena arena;
+    private Ball paddle1;
+    private Ball paddle2;
+    private Ball puck;
+    private Table table;
+    double friction = 1;
+    double changepuckX = 0;
+    double changepuckY = 0;
+    double puckspeed = 13;
+    private int score1 = 0;
+    private int score2 = 0;
+    private Text score1Display;
+    private Text score2Display;
+    private double initialPaddle1X;
+    private double initialPaddle1Y;
+    private double initialPaddle2X;
+    private double initialPaddle2Y;
+    private Text gameResult;
+    
+    //boolean variables for paddle movement
+    private static boolean paddle1Up = false;
+    private static boolean paddle1Down = false;
+    private static boolean paddle1Left = false;
+    private static boolean paddle1Right = false;
+    private static boolean paddle2Up = false;
+    private static boolean paddle2Down = false;
+    private static boolean paddle2Left = false;
+    private static boolean paddle2Right = false;
+
+    public GameLogic(GameArena arena, Table table) {
+        this.arena = arena;
+        this.table = table;
+        initializePaddlesandPuck();
+        // score
+        initializeScores();
+    }
+
+    private void initializeScores() {
+        score1Display = new Text("Player 1 Score: " + score1, 20, 100, 70, "WHITE");
+        score2Display = new Text("Player 2 Score: " + score2, 20, 700, 70, "WHITE");
+        
+        gameResult = new Text("", 35, 350, 350, "BLUE");
+        arena.addText(gameResult);
+        arena.addText(score1Display);
+        arena.addText(score2Display);
+    }
+
+    private void initializePaddlesandPuck() {
+
+        int paddleRadius = 50;
+        int paddleDistanceFromCenter = (arena.getArenaWidth() / 2) - paddleRadius;
+        // Calculate the positions of the paddles
+        int paddle1X = arena.getArenaWidth() / 2 - paddleDistanceFromCenter / 2;
+        int paddle2X = arena.getArenaWidth() / 2 + paddleDistanceFromCenter / 2;
+        // paddles
+        paddle1 = new Ball(paddle1X, arena.getArenaHeight() / 2, paddleRadius, "GREEN");
+        paddle2 = new Ball(paddle2X, arena.getArenaHeight() / 2, paddleRadius, "GREEN");
+        arena.addBall(paddle1);
+        arena.addBall(paddle2);
+
+        initialPaddle1X = paddle1.getXPosition();
+        initialPaddle1Y = paddle1.getYPosition();
+        initialPaddle2X = paddle2.getXPosition();
+        initialPaddle2Y = paddle2.getYPosition();
+
+        // puck
+        puck = new Ball(table.getTableCenterX(), table.getTableCenterY(), 20, "RED");
+        arena.addBall(puck);
+
+    }
+
+    public void updatePaddles() {
+        //control paddle movement
+        arena.getPanel().addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                // key pressed events
+                switch (evt.getKeyCode()) {
+                    case KeyEvent.VK_W:
+                        paddle1Up = true;
+                        break;
+                    case KeyEvent.VK_S:
+                        paddle1Down = true;
+                        break;
+                    case KeyEvent.VK_A:
+                        paddle1Left = true;
+                        break;
+                    case KeyEvent.VK_D:
+                        paddle1Right = true;
+                        break;
+                    case KeyEvent.VK_UP:
+                        paddle2Up = true;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        paddle2Down = true;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        paddle2Left = true;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        paddle2Right = true;
+                        break;
+                }
+            }
+
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                // key released events
+                switch (evt.getKeyCode()) {
+                    case KeyEvent.VK_W:
+                        paddle1Up = false;
+                        break;
+                    case KeyEvent.VK_S:
+                        paddle1Down = false;
+                        break;
+                    case KeyEvent.VK_A:
+                        paddle1Left = false;
+                        break;
+                    case KeyEvent.VK_D:
+                        paddle1Right = false;
+                        break;
+                    case KeyEvent.VK_UP:
+                        paddle2Up = false;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        paddle2Down = false;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        paddle2Left = false;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        paddle2Right = false;
+                        break;
+                }
+            }
+        });
+        // Set focus to the GameArena panel
+        arena.getPanel().setFocusable(true);
+        arena.getPanel().requestFocus();
+    }
+
+    public void startGameLoop() {
+        while (true) {
+            friction = friction * 0.60;
+            // Update paddle positions based on movement variables
+            if (paddle1Up && paddle1.getYPosition() - paddle1.getSize() / 2 > table.getTableCenterY()
+                    - table.getTableHeight() / 2) {
+                paddle1.setYPosition(paddle1.getYPosition() - 6);
+            }
+            if (paddle1Down && paddle1.getYPosition() + paddle1.getSize() / 2 < table.getTableCenterY()
+                    + table.getTableHeight() / 2) {
+                paddle1.setYPosition(paddle1.getYPosition() + 6);
+            }
+            if (paddle1Left && paddle1.getXPosition() - paddle1.getSize() / 2 > table.getTableCenterX()
+                    - table.getTableWidth() / 2) {
+                paddle1.setXPosition(paddle1.getXPosition() - 6);
+            }
+            if (paddle1Right && paddle1.getXPosition() + paddle1.getSize() / 2 < table.getTableCenterX()) {
+                paddle1.setXPosition(paddle1.getXPosition() + 6);
+            }
+            if (paddle2Up && paddle2.getYPosition() - paddle2.getSize() / 2 > table.getTableCenterY()
+                    - table.getTableHeight() / 2) {
+                paddle2.setYPosition(paddle2.getYPosition() - 6);
+            }
+            if (paddle2Down && paddle2.getYPosition() + paddle2.getSize() / 2 < table.getTableCenterY()
+                    + table.getTableHeight() / 2) {
+                paddle2.setYPosition(paddle2.getYPosition() + 6);
+            }
+            if (paddle2Left && paddle2.getXPosition() - paddle2.getSize() / 2 > table.getTableCenterX()) {
+                paddle2.setXPosition(paddle2.getXPosition() - 6);
+            }
+            if (paddle2Right && paddle2.getXPosition() + paddle2.getSize() / 2 < table.getTableCenterX()
+                    + table.getTableWidth() / 2) {
+                paddle2.setXPosition(paddle2.getXPosition() + 6);
+            }
+
+            // Detect puck collision
+            if (puck.collides(paddle1)) {
+                double angle = Math.atan2(puck.getYPosition() - paddle1.getYPosition(),
+                puck.getXPosition() - paddle1.getXPosition());
+                changepuckX = puckspeed * Math.cos(angle);
+                changepuckY = puckspeed * Math.sin(angle);
+            }
+
+            if (puck.collides(paddle2)) {
+                double angle = Math.atan2(puck.getYPosition() - paddle2.getYPosition(),
+                puck.getXPosition() - paddle2.getXPosition());
+                changepuckX = puckspeed * Math.cos(angle);
+                changepuckY = puckspeed * Math.sin(angle);
+            }
+
+            // Detect collision with the table boundaries
+            if (puck.getXPosition() - puck.getSize() / 2 <= table.getTableCenterX() - table.getTableWidth() / 2 ||
+                    puck.getXPosition() + puck.getSize() / 2 >= table.getTableCenterX() + table.getTableWidth() / 2) {
+                changepuckX = -changepuckX;
+            }
+            if (puck.getYPosition() - puck.getSize() / 2 <= table.getTableCenterY() - table.getTableHeight() / 2 ||
+                    puck.getYPosition() + puck.getSize() / 2 >= table.getTableCenterY() + table.getTableHeight() / 2) {
+                changepuckY = -changepuckY;
+            }
+
+            // Detect goal
+            if (isInGoal(puck, table.getGoal1())) {
+                score2++; // Player 2 scores
+                score2Display.setText("Player 2 Score: " + score2);
+                resetPuck(1);
+                resetPaddles();
+                if(score2 == 6) {
+                    gameResult.setText("Player 2 Won!");
+                    break;
+                }
+                
+            } else if (isInGoal(puck, table.getGoal2())) {
+                score1++; // Player 1 scores
+                score1Display.setText("Player 1 Score: " + score1);
+                resetPuck(2);
+                resetPaddles();
+                if(score1 == 6) {
+                    gameResult.setText("Player 1 Won!");
+                    break;
+                }
+
+            }
+
+            puck.move(changepuckX * friction, changepuckY * friction);
+            friction = 1;
+            arena.pause();
+        }
+    }
+
+    private boolean isInGoal(Ball puck, Rectangle goal) {
+        // Calculate the edges of the puck and goal
+        double puckX = puck.getXPosition();
+        double puckY = puck.getYPosition();
+        double goalLeft = goal.getXPosition();
+        double goalRight = goal.getXPosition() + goal.getWidth();
+        double goalTop = goal.getYPosition();
+        double goalBottom = goal.getYPosition() + goal.getHeight();
+
+        // Return true if the puck is inside the goal
+        return puckX > goalLeft && puckX < goalRight && puckY > goalTop && puckY < goalBottom;
+    }
+
+    private void resetPuck(int scoredOnPlayer) {
+        // Reset the puck position and velocity
+        if(scoredOnPlayer == 1){
+            puck.setXPosition(table.getTableCenterX() - 70);
+        }else {
+            puck.setXPosition(table.getTableCenterX() + 70);
+        }
+        puck.setYPosition(table.getTableCenterY());
+        changepuckX = 0;
+        changepuckY = 0;
+    }
+    //reset paddels
+    private void resetPaddles() {
+        paddle1.setXPosition(initialPaddle1X);
+        paddle1.setYPosition(initialPaddle1Y);
+        paddle2.setXPosition(initialPaddle2X);
+        paddle2.setYPosition(initialPaddle2Y);
+    }
+}
