e23e1a8011e1c49ba25d71cc58a437ab6b76cbe7
MurtazaGhous <m.ghous1@lancaster.ac.uk>
2023-05-22 01:02:04 +0100

    Updated gamelogic with sound and restart

--- GameLogic.java
+++ GameLogic.java
@@ -1,7 +1,7 @@
 import java.awt.event.KeyEvent;
 
 public class GameLogic {
-    
+
     private GameArena arena;
     private Ball paddle1;
     private Ball paddle2;
@@ -10,7 +10,7 @@ public class GameLogic {
     double friction = 1;
     double changepuckX = 0;
     double changepuckY = 0;
-    double puckspeed = 13;
+    double puckspeed = 15;
     private int score1 = 0;
     private int score2 = 0;
     private Text score1Display;
@@ -19,9 +19,16 @@ public class GameLogic {
     private double initialPaddle1Y;
     private double initialPaddle2X;
     private double initialPaddle2Y;
+
+    // Text
     private Text gameResult;
-    
-    //boolean variables for paddle movement
+    private Text restartText;
+    private Text mutetText;
+
+    private boolean gameRestart = false;
+    private boolean isMuted = false;
+
+    // boolean variables for paddle movement
     private static boolean paddle1Up = false;
     private static boolean paddle1Down = false;
     private static boolean paddle1Left = false;
@@ -31,22 +38,40 @@ public class GameLogic {
     private static boolean paddle2Left = false;
     private static boolean paddle2Right = false;
 
+    // sound
+    private SoundManager SoundManager = new SoundManager();
+
     public GameLogic(GameArena arena, Table table) {
+        // sound
+        initializeSound();
         this.arena = arena;
         this.table = table;
         initializePaddlesandPuck();
         // score
         initializeScores();
+
     }
 
     private void initializeScores() {
         score1Display = new Text("Player 1 Score: " + score1, 20, 100, 70, "WHITE");
         score2Display = new Text("Player 2 Score: " + score2, 20, 700, 70, "WHITE");
-        
-        gameResult = new Text("", 35, 350, 350, "BLUE");
+
+        gameResult = new Text("", 35, 370, 330, "BLUE");
+        restartText = new Text("", 25, 350, 360, "RED");
+        mutetText = new Text("To mute/unmute sound press M", 20, 350, 650, "RED");
         arena.addText(gameResult);
+        arena.addText(restartText);
         arena.addText(score1Display);
         arena.addText(score2Display);
+        arena.addText(mutetText);
+    }
+
+    private void initializeSound() {
+        SoundManager.preloadSound("hit.wav");
+        SoundManager.preloadSound("bounce.wav");
+        SoundManager.preloadSound("applause.wav");
+        SoundManager.preloadSound("drumroll.wav");
+        SoundManager.preloadSound("fanfare.wav");
     }
 
     private void initializePaddlesandPuck() {
@@ -74,7 +99,7 @@ public class GameLogic {
     }
 
     public void updatePaddles() {
-        //control paddle movement
+        // control paddle movement
         arena.getPanel().addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 // key pressed events
@@ -103,6 +128,18 @@ public class GameLogic {
                     case KeyEvent.VK_RIGHT:
                         paddle2Right = true;
                         break;
+                    case KeyEvent.VK_Y:
+                        gameRestart = true;
+                        break;
+                    case KeyEvent.VK_M:
+                        isMuted = !isMuted;
+                        if (isMuted) {
+                            SoundManager.mute();
+                        } else {
+                            SoundManager.unmute();
+                        }
+                        break;
+
                 }
             }
 
@@ -133,15 +170,23 @@ public class GameLogic {
                     case KeyEvent.VK_RIGHT:
                         paddle2Right = false;
                         break;
+                    case KeyEvent.VK_Y:
+                        gameRestart = false;
+                        break;
                 }
             }
         });
+
         // Set focus to the GameArena panel
         arena.getPanel().setFocusable(true);
         arena.getPanel().requestFocus();
     }
 
     public void startGameLoop() {
+        // Play the start music
+        if (!isMuted) {
+            SoundManager.playSound("fanfare.wav");
+        }
         while (true) {
             friction = friction * 0.60;
             // Update paddle positions based on movement variables
@@ -178,26 +223,38 @@ public class GameLogic {
 
             // Detect puck collision
             if (puck.collides(paddle1)) {
+                if (!isMuted) {
+                    SoundManager.playSound("hit.wav");
+                }
                 double angle = Math.atan2(puck.getYPosition() - paddle1.getYPosition(),
-                puck.getXPosition() - paddle1.getXPosition());
+                        puck.getXPosition() - paddle1.getXPosition());
                 changepuckX = puckspeed * Math.cos(angle);
                 changepuckY = puckspeed * Math.sin(angle);
+
             }
 
             if (puck.collides(paddle2)) {
+                if (!isMuted) {
+                    SoundManager.playSound("hit.wav");
+                }
                 double angle = Math.atan2(puck.getYPosition() - paddle2.getYPosition(),
-                puck.getXPosition() - paddle2.getXPosition());
+                        puck.getXPosition() - paddle2.getXPosition());
                 changepuckX = puckspeed * Math.cos(angle);
                 changepuckY = puckspeed * Math.sin(angle);
+
             }
 
             // Detect collision with the table boundaries
             if (puck.getXPosition() - puck.getSize() / 2 <= table.getTableCenterX() - table.getTableWidth() / 2 ||
                     puck.getXPosition() + puck.getSize() / 2 >= table.getTableCenterX() + table.getTableWidth() / 2) {
+                if (!isMuted) {
+                    SoundManager.playSound("bounce.wav");
+                }
                 changepuckX = -changepuckX;
             }
             if (puck.getYPosition() - puck.getSize() / 2 <= table.getTableCenterY() - table.getTableHeight() / 2 ||
                     puck.getYPosition() + puck.getSize() / 2 >= table.getTableCenterY() + table.getTableHeight() / 2) {
+                SoundManager.playSound("bounce.wav");
                 changepuckY = -changepuckY;
             }
 
@@ -207,19 +264,45 @@ public class GameLogic {
                 score2Display.setText("Player 2 Score: " + score2);
                 resetPuck(1);
                 resetPaddles();
-                if(score2 == 6) {
-                    gameResult.setText("Player 2 Won!");
-                    break;
+                if (!isMuted && score2 != 6) {
+                    SoundManager.playSound("applause.wav");
                 }
-                
+                if (score2 == 6) {
+                    if (!isMuted) {
+                        SoundManager.playSound("drumroll.wav");
+                    }
+                    gameResult.setText("Player 2 Won!");
+                    restartText.setText("Press Y to play again!");
+                    while (true) {
+                        arena.pause(); // pause the game
+                        if (gameRestart) {
+                            resetGame();
+                            break;
+                        }
+                    }
+                }
+
             } else if (isInGoal(puck, table.getGoal2())) {
                 score1++; // Player 1 scores
                 score1Display.setText("Player 1 Score: " + score1);
                 resetPuck(2);
                 resetPaddles();
-                if(score1 == 6) {
+                if (!isMuted && score1 != 6) {
+                    SoundManager.playSound("applause.wav");
+                }
+                if (score1 == 6) {
+                    if (!isMuted) {
+                        SoundManager.playSound("drumroll.wav");
+                    }
                     gameResult.setText("Player 1 Won!");
-                    break;
+                    restartText.setText("Press Y to play again!");
+                    while (true) {
+                        arena.pause(); // pause the game
+                        if (gameRestart) {
+                            resetGame();
+                            break;
+                        }
+                    }
                 }
 
             }
@@ -231,34 +314,55 @@ public class GameLogic {
     }
 
     private boolean isInGoal(Ball puck, Rectangle goal) {
-        // Calculate the edges of the puck and goal
-        double puckX = puck.getXPosition();
-        double puckY = puck.getYPosition();
+        // Find edges of the puck goal
+        double puckLeft = puck.getXPosition() - puck.getSize() / 2;
+        double puckRight = puck.getXPosition() + puck.getSize() / 2;
+        double puckTop = puck.getYPosition() - puck.getSize() / 2;
+        double puckBottom = puck.getYPosition() + puck.getSize() / 2;
         double goalLeft = goal.getXPosition();
         double goalRight = goal.getXPosition() + goal.getWidth();
         double goalTop = goal.getYPosition();
         double goalBottom = goal.getYPosition() + goal.getHeight();
-
         // Return true if the puck is inside the goal
-        return puckX > goalLeft && puckX < goalRight && puckY > goalTop && puckY < goalBottom;
+        return (puckLeft < goalRight && puckRight > goalLeft) &&
+                (puckTop < goalBottom && puckBottom > goalTop);
     }
 
     private void resetPuck(int scoredOnPlayer) {
-        // Reset the puck position and velocity
-        if(scoredOnPlayer == 1){
+        // Reset the puck position and speed
+        if (scoredOnPlayer == 1) {
             puck.setXPosition(table.getTableCenterX() - 70);
-        }else {
+        } else if (scoredOnPlayer == 2) {
             puck.setXPosition(table.getTableCenterX() + 70);
+        } else {
+            puck.setXPosition(table.getTableCenterX());
         }
         puck.setYPosition(table.getTableCenterY());
         changepuckX = 0;
         changepuckY = 0;
     }
-    //reset paddels
+
+    // reset paddels
     private void resetPaddles() {
         paddle1.setXPosition(initialPaddle1X);
         paddle1.setYPosition(initialPaddle1Y);
         paddle2.setXPosition(initialPaddle2X);
         paddle2.setYPosition(initialPaddle2Y);
+    }
+
+    // restart game
+    public void resetGame() {
+        score1 = 0;
+        score2 = 0;
+        score1Display.setText("Player 1 Score: " + score1);
+        score2Display.setText("Player 2 Score: " + score2);
+        gameResult.setText("");
+        restartText.setText("");
+        resetPuck(0);
+        resetPaddles();
+        gameRestart = false;
+        if (!isMuted) {
+            SoundManager.playSound("fanfare.wav");
+        }
     }
 }
